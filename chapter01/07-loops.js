ab5c30976fed12865ff0079683bc4f9c7c71f6a6
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-14 03:28:21 -0300

    loops

---
+++ chapter01/07-loops.js
@@ -0,0 +1,15 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
